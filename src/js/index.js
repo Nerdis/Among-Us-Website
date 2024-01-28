baf153051c589e396807af6306046dc9e3de872d
Nerdis <157893179+Nerdis@users.noreply.github.com>
2024-01-27 23:45:08 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+const botao = document.querySelector(".botao-plataforma");
+
+//////////////////////////////////////////////////////////
+
+const elementoPlataformas = document.querySelector(".botao-plataforma .plataformas");
+
+//////////////////////////////////////////////////////////
+
+botao.addEventListener("click", () => {
+
+ /////////////////////////////////////////////////////////
+
+    elementoPlataformas.classList.toggle("ativo");
+});
